d23ecee438d229df49036b17c2465d371a7fcb3f
Eldar <9eldik7@gmail.com>
2022-11-03 22:47:04 +0300

    add TDD when socket throw exception

--- src/test/java/client/DataSenderTest.java
+++ src/test/java/client/DataSenderTest.java
@@ -8,17 +8,29 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DataSenderTest {
 
+    private DatagramSocket socketMock;
+    private InetAddress address;
+    private byte[] expected;
+    private DataSender dataSender;
+
+    @Before
+    public void setup() throws UnknownHostException {
+        socketMock = mock(DatagramSocket.class);
+        address = ValuesGenerator.getRandomInetAddress();
+        expected = ValuesGenerator.getRandomByteArray();
+        dataSender = new DataSender();
+    }
+
     @Test
     public void whenDataSenderSendDataShouldSocketSendCorrectData() throws IOException {
-        DatagramSocket socketMock = mock(DatagramSocket.class);
-        byte[] expected = ValuesGenerator.getRandomByteArray();
         int port = 6666;
 
-        InetAddress address = ValuesGenerator.getRandomInetAddress();
         doAnswer(invocationOnMock -> {
             DatagramPacket datagramPacket = invocationOnMock.getArgument(0);
 
@@ -28,8 +40,18 @@ public class DataSenderTest {
             return null;
         }).when(socketMock).send(any(DatagramPacket.class));
 
-        DataSender dataSender = new DataSender();
         dataSender.sendData(socketMock, address, port, expected);
         verify(socketMock, times(1)).send(any());
+    }
+
+    @Test
+    public void whenInDataSenderSocketThrowAExceptionShouldTheExceptionPST() throws IOException {
+        IOException ioExceptionSpy = spy(IOException.class);
+        doThrow(ioExceptionSpy).when(socketMock).send(any());
+
+        dataSender.sendData(socketMock, address, 6666, expected);
+
+        verify(socketMock, times(1)).send(any());
+        verify(ioExceptionSpy, atLeast(1)).printStackTrace();
     }
 }
